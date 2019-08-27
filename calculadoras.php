2f59c850e76952de0d143b1ef738516aff374a86
Luis I. Reyes-Castro <luis.i.reyes.castro@gmail.com>
2019-08-27 16:51:46 -0500

    Reemplaza todas las tabulaciones por 4 espacios en blanco para que el codigo se lea bien en gitHub.

--- calculadoras.php
+++ calculadoras.php
@@ -2,38 +2,38 @@
 
 function evaluarTasaInternaRetorno( $rentas, $tasaMensual)
 {
-	$valor = 0.00;
-	foreach ( $rentas as $k => $renta ) {
-		$valor += $renta / ( ( 1.00 + $tasaMensual ) ** $k );
-	}
-	return $valor;
+    $valor = 0.00;
+    foreach ( $rentas as $k => $renta ) {
+        $valor += $renta / ( ( 1.00 + $tasaMensual ) ** $k );
+    }
+    return $valor;
 }
 
 function calcularDatosCredito( $principal, $tasaAnualNominal, $plazoMeses)
 {
-	
-	$tasaMensual = $tasaAnualNominal / 12.00 / 100.00;
+    
+    $tasaMensual = $tasaAnualNominal / 12.00 / 100.00;
 
-	// Calcula la cuota mensual usado la formula para EMI
-	// (ver: https://en.wikipedia.org/wiki/Equated_monthly_installment)
-	$eta 	= ( 1.00 + $tasaMensual ) ** $plazoMeses;
-	$cuota 	= ( $principal * $tasaMensual * $eta ) / ( $eta - 1.00 );
-	
+    // Calcula la cuota mensual usado la formula para EMI
+    // (ver: https://en.wikipedia.org/wiki/Equated_monthly_installment)
+    $eta     = ( 1.00 + $tasaMensual ) ** $plazoMeses;
+    $cuota     = ( $principal * $tasaMensual * $eta ) / ( $eta - 1.00 );
+    
     $cuota               = round( $cuota, 2);
     $totalPagos          = round( $cuota * $plazoMeses, 2);
     $totalIntereses      = round( $totalPagos - $principal, 2);
-	$interesSobreCapital = round( 100.00 * $totalIntereses / $principal, 2);
+    $interesSobreCapital = round( 100.00 * $totalIntereses / $principal, 2);
 
-	$intereses 	= array(0.00);
-	$capitales 	= array(0.00);
-	$insolutos	= array($principal);
-	$comisiones = array(0.00);
-	$rentas		= array(0.00);
+    $intereses     = array(0.00);
+    $capitales     = array(0.00);
+    $insolutos    = array($principal);
+    $comisiones = array(0.00);
+    $rentas        = array(0.00);
 
-	// Calcula recursivamente los pagos, las comisiones y las rentas
-	require 'tasaComision.php';
-	for( $k = 1; $k <= $plazoMeses; $k++)
-	{
+    // Calcula recursivamente los pagos, las comisiones y las rentas
+    require 'tasaComision.php';
+    for( $k = 1; $k <= $plazoMeses; $k++)
+    {
         $intereses[$k] = round( $insolutos[$k-1] * $tasaMensual, 2);
         $capitales[$k] = round( $cuota - $intereses[$k], 2);
         $insolutos[$k] = round( $insolutos[$k-1] - $capitales[$k], 2);
@@ -42,50 +42,50 @@ function calcularDatosCredito( $principal, $tasaAnualNominal, $plazoMeses)
             $insolutos[$k] = 0.00;
         }
         $comisiones[$k] = round( $insolutos[$k] * $tasaComision, 2);
-        $rentas[$k] 	= round( $cuota - $comisiones[$k], 2);
-	}
+        $rentas[$k]     = round( $cuota - $comisiones[$k], 2);
+    }
     $totalComisiones = array_sum($comisiones);
     $totalRentas     = array_sum($rentas);
     $ganancia             = $totalRentas - $principal;
     $gananciaSobreCapital = round( 100.00 * $ganancia / $principal, 2);
 
-	// Calcula la Tasa Interna de Retorno (TIR) mensual mediante biseccion
-	// usando la tasa mensual nominal como acota superior
-	require "precisionMetodoBiseccion.php";
+    // Calcula la Tasa Interna de Retorno (TIR) mensual mediante biseccion
+    // usando la tasa mensual nominal como acota superior
+    require "precisionMetodoBiseccion.php";
     $t_min = 0.00;
     $t_max = $tasaMensual;
-	while ( $t_max - $t_min > $precisionMetodoBiseccion )
-	{
-		$t_med = ( $t_max + $t_min ) / 2.00;
-		$valor_presente = 
-		evaluarTasaInternaRetorno( $rentas, $t_med);
-		if ( $valor_presente < $principal )
-		{
-			$t_max = $t_med;
-		}
-		else if( $valor_presente > $principal )
-		{
-			$t_min = $t_med;
-		}
-		else
-		{
-			break;
-		}
-	}
-	$tasaInternaRetorno = round( $t_med * 12.00 * 100.00, 2);
+    while ( $t_max - $t_min > $precisionMetodoBiseccion )
+    {
+        $t_med = ( $t_max + $t_min ) / 2.00;
+        $valor_presente = 
+        evaluarTasaInternaRetorno( $rentas, $t_med);
+        if ( $valor_presente < $principal )
+        {
+            $t_max = $t_med;
+        }
+        else if( $valor_presente > $principal )
+        {
+            $t_min = $t_med;
+        }
+        else
+        {
+            break;
+        }
+    }
+    $tasaInternaRetorno = round( $t_med * 12.00 * 100.00, 2);
 
-	// Almacena todos los datos en una sola variable tipo arreglo
-	$datosCredito = array();
-	$datosCredito['Cuota']                = $cuota;
-	$datosCredito['TotalPagos']           = $totalPagos;
+    // Almacena todos los datos en una sola variable tipo arreglo
+    $datosCredito = array();
+    $datosCredito['Cuota']                = $cuota;
+    $datosCredito['TotalPagos']           = $totalPagos;
     $datosCredito['TotalIntereses']       = $totalIntereses;
-	$datosCredito['InteresSobreCapital']  = $interesSobreCapital;
+    $datosCredito['InteresSobreCapital']  = $interesSobreCapital;
     $datosCredito['TotalComisiones']      = $totalComisiones;
     $datosCredito['TotalRentas']          = $totalRentas;
     $datosCredito['Ganancia']             = $ganancia;
     $datosCredito['GananciaSobreCapital'] = $gananciaSobreCapital;
     $datosCredito['TasaInternaRetorno']   = $tasaInternaRetorno;
-	$tablaSolicitantes                    = array();
+    $tablaSolicitantes                    = array();
     $tablaCrece                           = array();
     $tablaInversionistas                  = array();
     
@@ -126,10 +126,10 @@ function calcularDatosCredito( $principal, $tasaAnualNominal, $plazoMeses)
     }
 
     $datosCredito['TablaSolicitantes']   = $tablaSolicitantes;
-	$datosCredito['TablaCrece'] 	     = $tablaCrece;
-	$datosCredito['TablaInversionistas'] = $tablaInversionistas;
+    $datosCredito['TablaCrece']          = $tablaCrece;
+    $datosCredito['TablaInversionistas'] = $tablaInversionistas;
 
-	return $datosCredito;
+    return $datosCredito;
 
 }
 
