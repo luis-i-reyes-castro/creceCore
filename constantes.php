98d06f256596a33b426de2b9a3be3da1a6fe7dc0
Luis I. Reyes-Castro <luis.i.reyes.castro@gmail.com>
2019-08-29 08:52:37 -0500

    Agrega constante para la tasa del IVA.

--- constantes.php
+++ constantes.php
@@ -1,8 +1,10 @@
 <?php
-// Tasa de adjudicacion (0.80% del capital)
+// Tasa de adjudicacion
 $tasaAdjudicacion = 0.80 / 100.00;
-// Tasa de comision mensual (0.45% del capital insuluto)
+// Tasa de comision mensual
 $tasaComision     = 0.45 / 100.00;
+// Tasa del Impuesto al Valor Agregado (IVA)
+$tasaIVA          = 12.0 / 100.00;
 // Precision del metodo de biseccion utilizado para calcular la TIR
 $precisionMetodoBiseccion = 1E-6;
 ?>
