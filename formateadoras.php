50e2f033c7785e100466fa0648c1bec12a6eccf2
Luis I. Reyes-Castro <luis.i.reyes.castro@gmail.com>
2019-10-08 16:32:45 -0500

    En crearCopiaFormateada copia las columnas originales antes de formatearlas. Las columnas formateadas tienen los mismos nombres que las originales pero empiezan con el prefijo STR_.

--- formateadoras.php
+++ formateadoras.php
@@ -8,31 +8,55 @@ function crearCopiaFormateada( $tabla, $especificaciones)
         $nuevaTabla[$ind_fila] = array();
         foreach ( $especificaciones as $columna => $formato )
         {
-            $entrada = $fila[$columna];
+            $entradaOriginal = $fila[$columna];
+            $nuevaColumna    = 'STR_' . $columna;
+            $nuevaEntrada    = '';
+
             if ( $formato == '$' )
             {
-                $entrada = '$' . number_format( $entrada, 2);
+                $nuevaEntrada = '$' . number_format( $entradaOriginal, 2);
             }
             elseif ( $formato == '%' )
             {
-                $entrada = number_format( $entrada, 2) . '%';
+                $nuevaEntrada = number_format( $entradaOriginal, 2) . '%';
             }
             elseif ( $formato == 'm' )
             {
-                $entrada = strval($entrada) . ' meses';
+                $nuevaEntrada = strval($entradaOriginal) . ' meses';
             }
             elseif ( $formato == 'd' )
             {
-                $entrada = strval($entrada) . ' dias';
+                $nuevaEntrada = strval($entradaOriginal) . ' dias';
             }
             else
             {
-                $entrada = strval($entrada);
+                $nuevaEntrada = strval($entradaOriginal);
             }
-            $nuevaTabla[$ind_fila][$columna] = $entrada;
+
+            $nuevaTabla[$ind_fila][$columna]      = $entradaOriginal;
+            $nuevaTabla[$ind_fila][$nuevaColumna] = $nuevaEntrada;
+
         }
     }
     return $nuevaTabla;
 }
+
+// Para desarrollo/depuracion
+$tabla = array(
+         '1' => array( 'Name'=>'John Doe',
+                       'Salary'=>22525.99,
+                       'HireDate'=>'2012-12-31'),
+         '2' => array( 'Name'=>'Jane Roe',
+                       'Salary'=>40000.00,
+                       'HireDate'=>'1997-06-01'),
+         '3' => array( 'Name'=>'Jill Hill',
+                       'Salary'=>33600.10,
+                       'HireDate'=>'2008-01-25')
+         );
+$especificaciones = array( 'Name' => '',
+                           'Salary' => '$');
+
+$nuevaTabla = crearCopiaFormateada( $tabla, $especificaciones);
+print_r($nuevaTabla);
 
 ?>
